a9dcc52f0af8e27a2cb10c1687699fb274f8380d
AlbertoPMDM <albertopmdm@gmail.com>
2022-05-05 13:01:12 -0500

    added functions that save the state for cont use

--- src/python/msws32/msws32.py
+++ src/python/msws32/msws32.py
@@ -1,4 +1,5 @@
 from ctypes import c_uint64
+from sre_parse import State
 
 class msws32:
     '''
@@ -7,7 +8,14 @@ class msws32:
 
     _SEED = 0xb5ad4eceda1ce2a9
 
-    def randint32(n:int, seed:int = _SEED) -> int:
+    __slots__ = {
+        'state'
+    }
+
+    def __init__(self) -> None:
+        self.state = [0, 0]
+
+    def _randint32(n:int, seed:int = _SEED) -> int:
         '''
         generator type function that returns n random uints between 0 and 2^32
         '''
@@ -19,9 +27,33 @@ class msws32:
             x = c_uint64(x.value **2)
             w = c_uint64(w.value + s.value)
             x = c_uint64(x.value + w.value)
-            yield (x.value >> 32 | x.value << 32) & 0xFFFFFFFF
+            x = c_uint64((x.value >> 32 | x.value << 32) & 0xFFFFFFFF)
+            yield x.value
 
-    def rand32(min:float, max:float, n:int, seed:int = _SEED) -> float:
+    def randint32(self, n:int, state:list, seed:int = _SEED) -> int:
+        '''
+        generator type function that returns n random uints between 0 and 2^32
+        '''
+        x=c_uint64(state[0])
+        w=c_uint64(state[1])
+        s=c_uint64(seed)
+        for _ in range(n-1):
+            x = c_uint64(x.value)
+            x = c_uint64(x.value **2)
+            w = c_uint64(w.value + s.value)
+            x = c_uint64(x.value + w.value)
+            x = c_uint64((x.value >> 32 | x.value << 32) & 0xFFFFFFFF)
+            yield x.value
+        x = c_uint64(x.value)
+        x = c_uint64(x.value **2)
+        w = c_uint64(w.value + s.value)
+        x = c_uint64(x.value + w.value)
+        x = c_uint64((x.value >> 32 | x.value << 32) & 0xFFFFFFFF)
+        state[0] = x.value
+        state[1] = w.value
+        yield x.value
+
+    def _rand32(min:float, max:float, n:int, seed:int = _SEED) -> float:
         '''
         generator function that returns n random floats between min and max
         '''
@@ -35,6 +67,32 @@ class msws32:
             x = c_uint64(x.value + w.value)
             yield ((max-min)/(2**32)) * ((x.value >> 32 | x.value << 32) & 0xFFFFFFFF) + min
 
+    def rand32(self, min:float, max:float, n:int, state:list, seed:int = _SEED) -> int:
+        '''
+        generator type function that returns n random uints between 0 and 2^32
+        '''
+        x=c_uint64(state[0])
+        w=c_uint64(state[1])
+        s=c_uint64(seed)
+        for _ in range(n-1):
+            x = c_uint64(x.value)
+            x = c_uint64(x.value **2)
+            w = c_uint64(w.value + s.value)
+            x = c_uint64(x.value + w.value)
+            x = c_uint64((x.value >> 32 | x.value << 32) & 0xFFFFFFFF)
+            yield ((max-min)/(2**32)) * x.value + min
+        x = c_uint64(x.value)
+        x = c_uint64(x.value **2)
+        w = c_uint64(w.value + s.value)
+        x = c_uint64(x.value + w.value)
+        x = c_uint64((x.value >> 32 | x.value << 32) & 0xFFFFFFFF)
+        state[0] = x.value
+        state[1] = w.value
+        yield ((max-min)/(2**32)) * x.value + min
+    
 if __name__=='__main__':
-    for i in msws32.rand32(-1,1,10):
-        print(i)
+    rand = msws32()
+    for i, j in enumerate(rand.rand32(1, 2, 100, rand.state)):
+        print(f'{i}, {j}')
+    # a = [i for i in msws32.randint32(1)]
+    # print(a)
