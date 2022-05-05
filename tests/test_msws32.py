2996a81f2694807e58c2691f32437e67a09ec416
AlbertoPMDM <albertopmdm@gmail.com>
2022-05-05 01:18:22 -0500

    implemeted test for type of func and yield

---
+++ tests/test_msws32.py
@@ -0,0 +1,29 @@
+from types import GeneratorType
+import unittest
+from src.python.msws32.msws32 import msws32
+
+class test_msws64(unittest.TestCase):
+
+    def test_randint32(self):
+        #test for the func being a generator
+        self.assertEqual(
+            type((msws32.randint32(1))), 
+            GeneratorType
+            )
+        #test for the func returning int types
+        self.assertEqual(
+            type(next((msws32.randint32(1)))), 
+            int
+            )
+    
+    def test_rand32(self):
+        #test for the func being a generator
+        self.assertEqual(
+            type((msws32.rand32(0, 1, 1))), 
+            GeneratorType
+            )
+        #test for the func returning float types
+        self.assertEqual(
+            type(next((msws32.rand32(0, 1, 1)))), 
+            float
+            )
