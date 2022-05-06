983c85f45377b6aaac65e3864dedc2b6fb54e7c3
AlbertoPMDM <albertopmdm@gmail.com>
2022-05-06 00:02:25 -0500

    added mock for mc integration

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
-import src.python.basic as basic
-import src.python.example as example
+from src.python.msws32.msws32 import msws32
 import numpy as np
+import matplotlib.pyplot as plt
+import src.python.fmsws32 as fmsws32
 
-a = np.array([1,2,3,4,5])
+areas = fmsws32.msws.rand32(10)
+print(areas)
+print(type(areas))
+# rand = msws32()
+# a = 0
+# b = np.pi
+# n = 1000
+# xrand = [i for i in rand.rand32(a, b, n, rand.state)]
 
-a_o = example.array_func(a)
+# def f(x):
+#     return np.sin(x)
 
-print(a_o)
+# integral = 0
+# for i in xrand:
+#     integral += f(i)
 
-print(type(a_o))
+# answer = (b-a)/n*integral
+
+# areas = []
+# n_areas = 1000
+# for _ in range(n_areas):
+#     xrand = [i for i in rand.rand32(a, b, n, rand.state)]
+
+#     integral = 0
+#     for i in xrand:
+#         integral += f(i)
+#     answer = (b-a)/n*integral
+
+#     areas.append(answer)
+
+plt.title('Distribution of Areas Calculated')
+plt.hist(areas, bins = 30, ec = 'black')
+plt.xlabel('Areas')
+plt.ylabel('Freq')
+plt.show()
