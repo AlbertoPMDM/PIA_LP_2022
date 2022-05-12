2e1eeec38194a607ef26dcabd0ed96f3ce58d5f2
= <albertopmdm@gmail.com>
2022-05-12 12:51:44 -0500

    added timing decorator

---
+++ src/python/utils/utils.py
@@ -0,0 +1,12 @@
+from functools import wraps
+from time import time
+
+def timing(f:object)-> None:
+    @wraps(f)
+    def wrap(*args, **kw):
+        ts = time()
+        result = f(*args, **kw)
+        te = time()
+        print(f'func {f} took {te-ts}')
+        return result
+    return wrap
