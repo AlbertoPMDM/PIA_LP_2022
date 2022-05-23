8f5db6d312d658db933a2ba771d377f7a42ab43f
= <albertopmdm@gmail.com>
2022-05-22 19:56:32 -0500

    updated for new faverge

--- main.py
+++ main.py
@@ -3,37 +3,24 @@ import matplotlib.pyplot as plt
 
 from src.python.msws32.msws32 import msws32
 import src.python.xorshift256 as xs
+import src.python.faverage as fv
 
-a = np.zeros(100000)
-areas = xs.rnd(0,1,a)
-# rand = msws32()
-# a = 0
-# b = np.pi
-# n = 1000
-# xrand = [i for i in rand.rand32(a, b, n, rand.state)]
+a = 0
+b = np.pi
+n = 10000
+xrand = xs.rnd(a,b,np.zeros(n))
 
-# def f(x):
-#     return np.sin(x)
+def f(x):
+    return np.sin(x)
 
-# integral = 0
-# for i in xrand:
-#     integral += f(i)
+areas = []
+n_areas = 1000
 
-# answer = (b-a)/n*integral
+v_f = np.vectorize(f)
+for _ in range(n_areas):
+    areas.append(fv.faverage(a, b, v_f(xs.rnd(a,b,np.zeros(n)))))
 
-# areas = []
-# n_areas = 1000
-# for _ in range(n_areas):
-#     xrand = [i for i in rand.rand32(a, b, n, rand.state)]
-
-#     integral = 0
-#     for i in xrand:
-#         integral += f(i)
-#     answer = (b-a)/n*integral
-
-#     areas.append(answer)
 plt.title('Distribution of Areas Calculated')
 plt.hist(areas, bins = 30, ec = 'black')
 plt.xlabel('Areas')
 plt.ylabel('Freq')
-plt.show()
