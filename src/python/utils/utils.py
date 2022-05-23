312d4ab535f73b3b24c21d85025a538a3b79b210
AlbertoPMDM <albertopmdm@gmail.com>
2022-05-23 01:58:47 -0500

    timing func that returns time for testing

--- src/python/utils/utils.py
+++ src/python/utils/utils.py
@@ -10,3 +10,13 @@ def timing(f:object)-> None:
         print(f'func {f} took {te-ts}')
         return result
     return wrap
+
+def timingTime(f:object)-> None:
+    @wraps(f)
+    def wrap(*args, **kw):
+        ts = time()
+        result = f(*args, **kw)
+        te = time()
+        print(f'func {f} took {te-ts}')
+        return te-ts
+    return wrap
