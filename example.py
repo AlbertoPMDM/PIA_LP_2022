f0c7701018ed2356659916ba11a83e400e5ea086
= <albertopmdm@gmail.com>
2022-05-24 00:33:44 -0500

    changed example to another eq

--- example.py
+++ example.py
@@ -5,12 +5,14 @@ from src.python.msws32.msws32 import msws32
 import src.python.xorshift256 as xs
 import src.python.faverage as fv
 
-a = -1
-b = 1
+ax = -0.5
+bx = 0.5
+ay = -2
+by = 2
 n = 1000
 
 def f(x, y):
-    if x**2 + y**2 <= 1:
+    if (50/6)*x**2 + (1/2)*y**2 <= 2:
         return 1
     else:
         return 0
@@ -20,7 +22,7 @@ n_areas = 1000
 
 v_f = np.vectorize(f)
 for _ in range(n_areas):
-    areas.append(fv.faverage(2*a, 2*b, v_f( xs.rnd(a,b,np.zeros(n)), xs.rnd(a,b,np.zeros(n)) )))
+    areas.append(fv.faverage(ax+ay, bx+by, v_f( xs.rnd(ax,bx,np.zeros(n)), xs.rnd(ay,by,np.zeros(n)) )))
 
 print(np.average(areas))
 
